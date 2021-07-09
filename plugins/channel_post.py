a3befb064b5ba4b43ff8d0ea2c468a200339fa12
lullaby23 <84791701+lullaby23@users.noreply.github.com>
2021-07-09 08:25:36 +0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -25,9 +25,8 @@ async def channel_post(client: Client, message: Message):
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
 
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-
-    await reply_text.edit(f"{link}", reply_markup=reply_markup, disable_web_page_preview = True)
+   
+    await reply_text.edit(f"{link}", disable_web_page_preview = True)
 
     if not DISABLE_CHANNEL_BUTTON:
         await post_message.edit_reply_markup(reply_markup)
